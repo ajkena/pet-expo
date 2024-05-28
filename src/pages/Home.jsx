6cc383b70970c137daae6070768ca5f40c6fbfd6
ajkenalacka <ajkena.lacka@gmail.com>
2024-05-28 22:21:29 +0200

    add about us description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,15 @@ export default function Home() {
     <main>
       <section>
         <h1>About Us</h1>
+        <p>
+          Welcome to Pet Expo, the ultimate destination for pet enthusiasts! Our
+          website showcases a delightful array of cats, dogs, and birds,
+          complete with stunning photos and comprehensive information about each
+          breed. Whether you're a seasoned pet owner or a curious newcomer,
+          you'll find everything you need to know about your favorite furry and
+          feathered friends. Join us to explore the wonderful world of pets and
+          discover the perfect companion for your home!
+        </p>
       </section>
 
       <ContactUs />
